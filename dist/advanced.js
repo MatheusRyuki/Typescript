c59b3fbc9ea642186f65c85824c734f638905e70
Matheus Ryuki <dr_4_gao@hotmail.com>
2020-04-19 08:45:13 -0300

    Mais tipos de guardas

--- dist/advanced.js
+++ dist/advanced.js
@@ -4,3 +4,48 @@ var el = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+var adicionar = function (a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+};
+var printEmployeeInformation = function (emp) {
+    console.log("Nome: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privil\u00E9gios: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("In\u00EDcio em: " + emp.startDate);
+    }
+};
+printEmployeeInformation(el);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Dirigindo...");
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Dirigindo...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Carregando " + amount + " produtos");
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+var useVehicle = function (vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(100);
+    }
+};
+useVehicle(v1);
+useVehicle(v2);
--- src/advanced.ts
+++ src/advanced.ts
@@ -1,17 +1,73 @@
-interface Admin {
+type Admin = {
   name: string;
   privileges: string[];
 }
 
-interface Employee {
+type Employee = {
   name: string;
   startDate: Date;
 }
 
-interface ElevatedEmployee extends Admin, Employee {}
+type ElevatedEmployee = Admin & Employee;
 
 const el: ElevatedEmployee = {
   name: "Fernando",
   privileges: ["create-server"],
   startDate: new Date(),
 }
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+const adicionar = (a: Combinable, b: Combinable) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployee) => {
+  console.log(`Nome: ${emp.name}`);
+  if ("privileges" in emp) {
+    console.log(`Privilégios: ${emp.privileges}`)
+  }
+  if ("startDate" in emp) {
+    console.log(`Início em: ${emp.startDate}`)
+  }
+}
+
+printEmployeeInformation(el);
+
+class Car {
+  drive() {
+    console.log("Dirigindo...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Dirigindo...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Carregando ${amount} produtos`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
