619c4ae1e32dcc1eb296b0be4ff470fc581bd1b2
Matheus Ryuki <dr_4_gao@hotmail.com>
2020-04-21 07:35:21 -0300

    Trabalhando com constantes

--- src/generics.ts
+++ src/generics.ts
@@ -7,15 +7,19 @@ const promise: Promise<string> = new Promise((resolve) => {
   }, 2000);
 });
 
-promise.then(data => {
+promise.then((data) => {
   data.split(" ");
 });
 
 // Criando uma função genérica
 
-const merge = <T, U>(objA: T, objB: U) => {
+const merge = <T extends object, U extends object>(objA: T, objB: U) => {
   return Object.assign(objA, objB);
-}
+};
 
-const mergeObj = merge<{nome: string, hobby: string[]}, {idade: number}>({nome: "Max", hobby: ["Esportes"]}, {idade: 30});
+const mergeObj = merge<{ nome: string; hobby: string[] }, { idade: number }>(
+  { nome: "Max", hobby: ["Esportes"] },
+  { idade: 30 }
+);
 console.log(mergeObj.hobby);
+
