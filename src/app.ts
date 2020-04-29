a001db0ace317270546a6b4a0b91466c8b3057ff
Matheus Ryuki <dr_4_gao@hotmail.com>
2020-04-29 09:32:14 -0300

    Utilizando interfaces para implementar o drag and drop

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag e Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
   Active,
@@ -156,7 +168,8 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // Classe do ProjectItem
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable {
   private project: Project;
 
   get persons() {
@@ -167,6 +180,15 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     }
   }
 
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    alert(event);
+  }
+
+  dragEndHandler(event: DragEvent) {
+    alert(event);
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -175,7 +197,11 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent();
   }
 
-  configure() {}
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
     this.element.querySelector("h3")!.textContent = this.persons;
